851d2a9435a964f187d5c1027b191fecc3abd099
Zhangxia <Xiaohui.Zhang@unisys.com>
2019-11-28 11:12:40 +0800

    test

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
             />
             <i>哎呀</i>
             <i>nishi 2222222222</i>
+            <span>再来一个呀 嘿呀嘿呀</span>
             <Route path={`${process.env.PUBLIC_URL}/`} component={Home} />
           </Switch>
         </div>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import './styles/index.less';
 ReactDOM.render(<App />, document.getElementById('root'));
 //maomaomao
+// 什么操作
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
