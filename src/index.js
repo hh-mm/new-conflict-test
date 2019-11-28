eb5694a90d814d63ef0b3f127c5cea60543199b1
maoxinyu <maoxinyu@ysstech.com>
2019-11-28 11:15:44 +0800

    。。

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './styles/index.less';
 ReactDOM.render(<App />, document.getElementById('root'));
 //maomaomao
 // 什么操作
+// 你猜
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
